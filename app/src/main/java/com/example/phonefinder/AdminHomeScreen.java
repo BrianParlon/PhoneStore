01c636adf6ba79330e19ae7daf9a6d4addc41e55
Brian <b.parlon@gmail.com>
2023-04-07 16:18:24 +0100

    Added Home Screen for user and Admin no functionality

---
+++ app/src/main/java/com/example/phonefinder/AdminHomeScreen.java
@@ -0,0 +1,93 @@
+package com.example.phonefinder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+public class AdminHomeScreen extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
+    private Boolean isOpen=false;
+    Button btnPicture,newProd,saved,display;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        saved = (Button) findViewById(R.id.savedDimension);
+        saved.setOnClickListener(this);
+
+        newProd = (Button) findViewById(R.id.button5);
+        newProd.setOnClickListener(this);
+
+
+        display = (Button) findViewById(R.id.displayRoom);
+        display.setOnClickListener(this);
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference  = FirebaseDatabase.getInstance().getReference("admin");
+        userId = user.getUid();
+
+        TextView accName =(TextView) findViewById(R.id.textView2);
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Admin adminProfile = snapshot.getValue(Admin.class);
+
+                if(adminProfile != null){
+                    String accountName = adminProfile.getFirstName();
+                    accName.setText(accountName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AdminHomeScreen.this,"Error",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button lgOut = (Button) findViewById(R.id.logout);
+        lgOut.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+
+
+
+        }
+    }
+
+
+}
